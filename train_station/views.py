3ae554e6e6a82fd69ae54ebc29c4179d3ed9a329
Artem Milchenko <pl301078map2@gmail.com>
2024-03-26 19:16:33 +0200

    Created TrainTypeViewSet

--- train_station/views.py
+++ train_station/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from train_station.models import TrainType
+from train_station.serializers import TrainTypeSerializer
+
+
+class TrainTypeViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    GenericViewSet
+):
+    queryset = TrainType.objects.all()
+    serializer_class = TrainTypeSerializer
