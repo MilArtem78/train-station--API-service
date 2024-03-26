8ca47ced9a4ce356d23d31f23a0158fd5e41b470
Artem Milchenko <pl301078map2@gmail.com>
2024-03-26 19:31:16 +0200

    Created StationSerializer

--- train_station/serializers.py
+++ train_station/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
-from train_station.models import TrainType
+from train_station.models import TrainType, Station
 
 
 class TrainTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = TrainType
         fields = ("id", "name", "description")
+
+
+class StationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Station
+        fields = ("id", "name", "latitude", "longitude")
