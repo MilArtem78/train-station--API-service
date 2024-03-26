54c86433be633288f0e80fa687011036e5d2e6fa
Artem Milchenko <pl301078map2@gmail.com>
2024-03-26 14:32:05 +0200

    Created model Route

--- train_station/models.py
+++ train_station/models.py
@@ -6,5 +6,25 @@ class Station(models.Model):
     latitude = models.FloatField()
     longitude = models.FloatField()
 
+    class Meta:
+        ordering = ["name"]
+
     def __str__(self):
         return self.name
+
+
+class Route(models.Model):
+    source = models.ForeignKey(
+        Station, on_delete=models.CASCADE, related_name="source_routes"
+    )
+    destination = models.ForeignKey(
+        Station, on_delete=models.CASCADE, related_name="destination_routes"
+    )
+    distance = models.PositiveIntegerField()
+
+    class Meta:
+        unique_together = ["source", "destination"]
+        ordering = ["source"]
+
+    def __str__(self):
+        return f"{self.source} - {self.destination}"
