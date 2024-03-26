1aa16114589b13e44cb318a83c3c6f160a580270
Artem Milchenko <pl301078map2@gmail.com>
2024-03-26 19:32:21 +0200

    Registered URL endpoint for Station

--- train_station/urls.py
+++ train_station/urls.py
@@ -2,10 +2,14 @@ from django.urls import path
 from django.urls.conf import include
 from rest_framework import routers
 
-from train_station.views import TrainTypeViewSet
+from train_station.views import (
+    TrainTypeViewSet,
+    StationViewSet
+)
 
 router = routers.DefaultRouter()
 router.register("train_types", TrainTypeViewSet)
+router.register("stations", StationViewSet)
 
 urlpatterns = [path("", include(router.urls))]
 
